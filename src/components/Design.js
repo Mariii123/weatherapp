02d1a9293bb0bc6a5920804bb467e0356659fae8
Marishwaran <marishwaran.k@somaiya.edu>
2019-07-10 22:05:41 +0530

    Update

--- src/components/Design.js
+++ src/components/Design.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import axios from "axios";
 import logo from "../logo.svg";
 import night from "../moon.svg";
-
+import rain from "../rain.svg";
 const api_key = "208990df1dbbe8f38d20e0a14d7b4329";
 
 class Design extends Component {
@@ -17,6 +17,7 @@ class Design extends Component {
     pressure: ""
   };
   componentDidMount() {
+    console.log(this.props);
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         var { latitude, longitude } = position.coords;
@@ -27,7 +28,7 @@ class Design extends Component {
           .then(res => {
             var { data } = res;
             this.setState({
-              name: data.name,
+              city: data.name,
               degree: Math.round(data.main.temp - 273.15),
               description: data.weather[0].main,
               wind: data.wind.speed,
@@ -55,10 +56,9 @@ class Design extends Component {
           `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
         )
         .then(res => {
-          console.log(res.data);
           const { data } = res;
           this.setState({
-            name: data.name,
+            city: data.name,
             degree: Math.round(data.main.temp - 273.15),
             description: data.weather[0].main,
             wind: data.wind.speed,
@@ -72,51 +72,62 @@ class Design extends Component {
     }
   };
   render() {
+    var img = undefined;
+    if (this.state.description == "Drizzle") {
+      img = rain;
+    } else {
+      img = night;
+    }
     return (
-      <div className="container day-bg">
+      <div className="main-container day-bg">
         <div className="navbar">
-          <div className="logo">
-            <img className="App-logo" src={logo} alt="Logo" />
-          </div>
           <div>
-            <form onSubmit={this.handleSubmit}>
+            <p className="logo">
+              <i className="fa fa-bolt" /> weather
+            </p>
+          </div>
+          <i className="fa fa-plus" />
+        </div>
+        <div className="container">
+          <div className="city-date-container">
+            <form onSubmit={this.handleSubmit} className="inline">
               <input
-                onChange={this.handleChange}
-                id="city"
-                required
                 type="text"
+                className="city-name"
+                onChange={this.handleChange}
                 value={this.state.city}
-                placeholder="City Name / Pincode"
               />
-              <button type="submit">Find</button>
+              <button className="fa fa-arrow-right" type="submit" />
             </form>
+            <p className="date">Sunday, 7 July</p>
           </div>
-        </div>
-        <div>
-          <p className="city-name">{this.state.name}</p>
-          <p className="date">Sunday, 7 July</p>
-        </div>
-        <div className="weather-data">
-          <p className="weather-temp">{this.state.degree}°</p>
-          <div>
-            <img src={night} alt="Night" className="icon" />
-            <p className="weather-desc">{this.state.description}</p>
-          </div>
-          <div className="other-data">
-            <div className="card">
-              <p className="title-light">Wind</p>
-              <p className="data-value">{this.state.wind}</p>
-              <p className="data-unit">m/s</p>
-            </div>
-            <div className="card">
-              <p className="title-light">Humidity</p>
-              <p className="data-value">{this.state.humidity}</p>
-              <p className="data-unit">%</p>
-            </div>
-            <div className="card">
-              <p className="title-light">Pressure</p>
-              <p className="data-value">{this.state.pressure}</p>
-              <p className="data-unit">hPa</p>
+
+          <div className="weather-data">
+            <div>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <p className="weather-temp">{this.state.degree}°C</p>
+                <div>
+                  <img src={img} alt="Night" className="icon" />
+                  <p className="weather-desc">{this.state.description}</p>
+                </div>
+              </div>
+              <div className="other-data">
+                <div className="card">
+                  <p className="title-light">Wind</p>
+                  <p className="data-value">{this.state.wind}</p>
+                  <p className="data-unit">m/s</p>
+                </div>
+                <div className="card">
+                  <p className="title-light">Humidity</p>
+                  <p className="data-value">{this.state.humidity}</p>
+                  <p className="data-unit">%</p>
+                </div>
+                <div className="card">
+                  <p className="title-light">Pressure</p>
+                  <p className="data-value">{this.state.pressure}</p>
+                  <p className="data-unit">hPa</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
